5fcada3d4e75a44663f6824126de9d744f287272
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-09 03:07:02 +0630

    Update tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,7 +9,7 @@ Create Table medical_histories (
     admitted_at Timestamp,
     patient_id Int,
     status Varchar(100),
-    Constraint fk_medical_history_patients 
+    Constraint fk_medical_histories_patients 
 		Foreign Key (patient_id)
     		References patients(id)
 );
@@ -17,22 +17,18 @@ Create Table medical_histories (
 Create Table treatments (
     id Int Primary Key,
     type Varchar(100),
-    name Varchar(100),
-    medical_history_id Int,
-    Constraint fk_treatments_medical_history 
-		Foreign Key (medical_history_id)
-    		References medical_history(id)
+    name Varchar(100)
 );
 
 Create Table invoices (
     id Int Primary Key,
-    total_amount DecimaL(5, 2),
+    total_amount Decimal(5, 2),
     generated_at Timestamp,
     payed_at Timestamp,
     medical_history_id Int,
-    Constraint fk_invoices_medical_history 
+    Constraint fk_invoices_medical_histories 
 		Foreign Key (medical_history_id)
-    		References medical_history(id)
+    		References medical_histories(id)
 );
 
 Create Table invoice_items (
@@ -41,25 +37,27 @@ Create Table invoice_items (
     quantity Int,
     total_price Decimal(5, 2),
     invoice_id Int,
-    treatments_id Int,
+    treatment_id Int,
     Constraint fk_invoice_items_invoices 
-		Foreign Key (invoices_id)
+		Foreign Key (invoice_id)
     		References invoices(id),
-    CONSTRAINT fk_invoice_items_treatments 
-		Foreign Key (treatments_id)
+    Constraint fk_invoice_items_treatments 
+		Foreign Key (treatment_id)
     		References treatments(id)
 );
 
-Create Table patient_treatment (
-    patients_id Int,
+Create Table medical_histories_treatments (
+	id Int Primary Key,
+    medical_history_id Int,
     treatments_id Int,
-    Constraint fk_patient_treatment_patient 
-		Foreign Key (patients_id)
-    		References patients(id),
-    Constraint fk_patient_treatment_treatment 
+    Constraint fk_medical_histories_treatments_mh
+		Foreign Key (medical_history_id)
+    		References medical_histories(id),
+    Constraint fk_medical_histories_treatments_tr 
 		Foreign Key(treatments_id)
     		References treatments(id)
 );
+
 
 CREATE INDEX idx_patient_treatment ON patient_treatment(patients_id);
 CREATE INDEX idx_treatments_id ON patient_treatment(treatments_id);
